19b25f1df2feb3bcd42d57c9e8abf458ed7825b2
AlexLaptop <alma9011@colorado.edu>
2020-03-04 16:20:12 -0700

    3/4 ext. see readme

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ A website project for cs 3308!
 (2/28) I added a .gitignore. If you look inside it there is an ignore for .env files. This is where the process.env.VARIABLE's are coming from. Shoot me a text and I can tell you how to set    those up.
 (3/4) Added an openweather field in the env file. Created a seperate page for user registration and some form validation. User is now logged in once they register for an account. Made a mock 
 post route for getting weather data from openweather, currently it only gets data for the city of London.
+(3/4) ext. Added Google Maps Direction API test route (it works!), and added two buttons on the main page to test the routes.
+Also did some refactoring of the register route for a crash,
+and some logic for which warnings are more important (for example: password match vs password empty, which to show?). Also added
+some documenation using JSdoc styling.
 
 ## Getting up and running:
 1. Clone this repo
--- resources/js/register.js
+++ resources/js/register.js
@@ -1,21 +1,27 @@
+/**
+ * This function creates invalid and warning messages for the registration form!
+ * @param {String} message -This is passed by ejs, which gets it from the server. This is a message that correlates to a user error
+ * 
+ */
+
 function register_error(message) {
-    if (message === "invuser") {
+    if (message === "invuser" || message === "usernameempty") {
         document.getElementById("username-field").classList.add('is-invalid');
         let invText = document.createElement('p');
         invText.classList.add("text-danger");
         invText.classList.add("mb-0");
         invText.classList.add("mt-2");
-        invText.innerHTML="Username already taken!";
+        invText.innerHTML = (message === "invuser" ? "Username already taken!" : "Please enter a username!");
         document.getElementById("username-group").appendChild(invText);
     }
-    else if (message === "passwordmatch") {
+    else if (message === "passwordmatch" || message === "passwordempty") {
         document.getElementById("password-field").classList.add('is-invalid');
         document.getElementById("r_password-field").classList.add('is-invalid');
         let invText = document.createElement('p');
         invText.classList.add("text-danger");
         invText.classList.add("mb-0");
         invText.classList.add("mt-2");
-        invText.innerHTML="Passwords do not match!";
+        invText.innerHTML = (message === "passwordmatch" ? "Passwords do not match!" : "Please enter a password!");
         document.getElementById("password-group").appendChild(invText);
     }
 }
